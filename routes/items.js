c7c1f8f2fe6f908af576466ee561cc0963720671
Somsubhra Das <das.somsubhra1@gmail.com>
2021-06-19 11:45:07 +0530

    response options set

--- routes/items.js
+++ routes/items.js
@@ -1,13 +1,46 @@
 const items = require("../items");
 
 function itemRoutes(fastify, options, done) {
-  fastify.get("/items", (req, reply) => {
-    // Can also send response directly through return
-    // return items;
+  // Get all items
+
+  // options for get items
+
+  const getItemsOpts = {
+    schema: {
+      response: {
+        200: {
+          type: "array",
+          items: {
+            type: "object",
+            properties: {
+              id: { type: "string" },
+              name: { type: "string" },
+            },
+          },
+        },
+      },
+    },
+  };
+  fastify.get("/items", getItemsOpts, (req, reply) => {
     reply.send(items);
   });
 
-  fastify.get("/items/:id", (req, reply) => {
+  // Get single item
+
+  const getItemOpts = {
+    schema: {
+      response: {
+        200: {
+          type: "object",
+          properties: {
+            id: { type: "string" },
+            name: { type: "string" },
+          },
+        },
+      },
+    },
+  };
+  fastify.get("/items/:id", getItemOpts, (req, reply) => {
     const { id } = req.params;
 
     const item = items.find((item) => item.id === id);
