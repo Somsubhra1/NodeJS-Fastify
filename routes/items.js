9e02581142315bffa446c81d22cf3fc32ca75599
Somsubhra Das <das.somsubhra1@gmail.com>
2021-06-19 12:58:20 +0530

    options added in correct place

--- routes/items.js
+++ routes/items.js
@@ -34,6 +34,13 @@ const getItemOpts = {
 
 const postItemOpts = {
   schema: {
+    body: {
+      type: "object",
+      required: ["name"],
+      properties: {
+        name: { type: "string" },
+      },
+    },
     response: {
       201: Item,
     },
