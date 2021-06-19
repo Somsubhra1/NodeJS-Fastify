2f085fa050b5ba1791bad1f417cd9b5c88900dbd
Somsubhra Das <das.somsubhra1@gmail.com>
2021-06-19 11:50:45 +0530

    handlers added to options

--- routes/items.js
+++ routes/items.js
@@ -1,5 +1,13 @@
 const items = require("../items");
 
+const Item = {
+  type: "object",
+  properties: {
+    id: { type: "string" },
+    name: { type: "string" },
+  },
+};
+
 function itemRoutes(fastify, options, done) {
   // Get all items
 
@@ -8,45 +16,34 @@ function itemRoutes(fastify, options, done) {
   const getItemsOpts = {
     schema: {
       response: {
-        200: {
-          type: "array",
-          items: {
-            type: "object",
-            properties: {
-              id: { type: "string" },
-              name: { type: "string" },
-            },
-          },
-        },
+        200: Item,
       },
     },
+    handler: (req, reply) => {
+      reply.send(items);
+    },
   };
-  fastify.get("/items", getItemsOpts, (req, reply) => {
-    reply.send(items);
-  });
+
+  fastify.get("/items", getItemsOpts);
 
   // Get single item
 
   const getItemOpts = {
     schema: {
       response: {
-        200: {
-          type: "object",
-          properties: {
-            id: { type: "string" },
-            name: { type: "string" },
-          },
-        },
+        200: Item,
       },
     },
+    handler: (req, reply) => {
+      const { id } = req.params;
+
+      const item = items.find((item) => item.id === id);
+
+      reply.send(item);
+    },
   };
-  fastify.get("/items/:id", getItemOpts, (req, reply) => {
-    const { id } = req.params;
 
-    const item = items.find((item) => item.id === id);
-
-    reply.send(item);
-  });
+  fastify.get("/items/:id", getItemOpts);
 
   done();
 }
